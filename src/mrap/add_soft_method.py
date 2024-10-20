f37b6aea703fdaf32aaaa92f1af4fdbc3a008e1a
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-10-20 15:59:01 +0200

    adjust add_soft_method as a building block

--- src/mrap/add_soft_method.py
+++ src/mrap/add_soft_method.py
@@ -1,11 +1,14 @@
 import sys
-from dtreg.load_datatype import load_datatype
+from importlib.metadata import version
+
+dt = None
 
 
-def add_soft_method(method_name):
-    version = str(sys.version)[:5]
-    soft_all = load_datatype("https://doi.org/21.T11969/abef9f1c0d48853f3e51")
-    software = soft_all.software(label="Python", versioninfo=version)
-    soft_library = soft_all.software_library(label=method_name, part_of=software)
-    soft_method = soft_all.software_method(label=method_name, uses_software=soft_library)
+def add_soft_method(pack, fun):
+    global dt
+    version_Py = str(sys.version)[:5]
+    version_pack = version(pack)
+    software = dt.software(label="Python", versionInfo=version_Py)
+    soft_library = dt.software_library(label=pack, versionInfo=version_pack, part_of=software)
+    soft_method = dt.software_method(label=fun, uses_software=soft_library)
     return soft_method
--- tests/add_soft_method_tests.py
+++ tests/add_soft_method_tests.py
@@ -1,13 +1,16 @@
 import unittest
+from unittest.mock import patch
 from mrap.add_soft_method import add_soft_method
+from dtreg.load_datatype import load_datatype
 
 
 class TestSoftMethod(unittest.TestCase):
 
-    def test_soft_method_props(self):
-        soft_method_inst = add_soft_method("Wilcoxon")
-        props = ['label', 'uses_software', 'is_implemented_by', 'has_support_url']
-        self.assertEqual(soft_method_inst.prop_list, props)
+    def test_soft_method_label(self):
+        new_dt = load_datatype("https://doi.org/21.T11969/ff5e3f857788d20dd1aa")
+        with patch('mrap.add_soft_method.dt', new_dt):
+            soft_method_inst = add_soft_method("scipy", "f_oneway")
+            self.assertEqual(soft_method_inst.label, "f_oneway")
 
 
 if __name__ == '__main__':
