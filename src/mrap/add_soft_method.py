4f6de92fe6e9f9c941588324d0a4d2ac3c211d5b
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-01-30 20:42:38 +0100

    new add_soft_method and unit tests

--- src/mrap/add_soft_method.py
+++ src/mrap/add_soft_method.py
@@ -1,14 +1,12 @@
 import sys
 from importlib.metadata import version
 
-dt = None
 
-
-def add_soft_method(pack, fun):
-    global dt
-    version_Py = str(sys.version)[:5]
-    version_pack = version(pack)
-    software = dt.software(label="Python", versionInfo=version_Py)
-    soft_library = dt.software_library(label=pack, versionInfo=version_pack, part_of=software)
-    soft_method = dt.software_method(label=fun, uses_software=soft_library)
+def add_soft_method(dt, lib_fun):
+    lib = lib_fun.split(",")[0]
+    version_py = str(sys.version)[:5]
+    version_lib = version(lib)
+    software = dt.software(label="Python", versionInfo=version_py)
+    soft_library = dt.software_library(label=lib, versionInfo=version_lib, part_of=software)
+    soft_method = dt.software_method(label=lib_fun, part_of=soft_library)
     return soft_method
