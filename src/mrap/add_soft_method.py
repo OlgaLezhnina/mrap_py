3f890f0db4462852d67202dae0bfafe6bc45a44e
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-01-31 14:10:58 +0100

    new soft method label

--- src/mrap/add_soft_method.py
+++ src/mrap/add_soft_method.py
@@ -3,10 +3,11 @@ from importlib.metadata import version
 
 
 def add_soft_method(dt, lib_fun):
-    lib = lib_fun.split(",")[0]
+    lib = lib_fun.split(";")[0]
+    fun = lib_fun.split(";")[1]
     version_py = str(sys.version)[:5]
     version_lib = version(lib)
     software = dt.software(label="Python", versionInfo=version_py)
     soft_library = dt.software_library(label=lib, versionInfo=version_lib, part_of=software)
-    soft_method = dt.software_method(label=lib_fun, part_of=soft_library)
+    soft_method = dt.software_method(label=fun, part_of=soft_library)
     return soft_method
--- tests/add_soft_method_tests.py
+++ tests/add_soft_method_tests.py
@@ -6,17 +6,17 @@ from dtreg.load_datatype import load_datatype
 class TestSoftMethod(unittest.TestCase):
     def test_soft_method_inst_name(self):
         dt = load_datatype("https://doi.org/21.T11969/b9335ce2c99ed87735a6")
-        soft_method_inst = add_soft_method(dt, "scipy,f_oneway")
+        soft_method_inst = add_soft_method(dt, "scipy;f_oneway")
         self.assertEqual(dt.software_method.dt_name, soft_method_inst.dt_name)
 
     def test_soft_method_label(self):
         dt = load_datatype("https://doi.org/21.T11969/b9335ce2c99ed87735a6")
-        soft_method_inst = add_soft_method(dt, "scipy,f_oneway")
-        self.assertEqual(soft_method_inst.label, "scipy,f_oneway")
+        soft_method_inst = add_soft_method(dt, "scipy;f_oneway")
+        self.assertEqual(soft_method_inst.label, "f_oneway")
 
     def test_soft_method_lib_label(self):
         dt = load_datatype("https://doi.org/21.T11969/b9335ce2c99ed87735a6")
-        soft_method_inst = add_soft_method(dt, "scipy,f_oneway")
+        soft_method_inst = add_soft_method(dt, "scipy;f_oneway")
         self.assertEqual(soft_method_inst.part_of.label, "scipy")
 
 
