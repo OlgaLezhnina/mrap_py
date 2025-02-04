02965cb13cf121a2efca22b546a74ae531124383
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-02-04 21:17:01 +0100

    add scipy_f_oneway wrapper function

---
+++ src/mrap/scipy_f_oneway.py
@@ -0,0 +1,37 @@
+from .add_soft_method import add_soft_method
+from dtreg.load_datatype import load_datatype
+from dtreg.to_jsonld import to_jsonld
+import pandas as pd
+from scipy.stats import f_oneway
+from varname import argname
+
+
+def scipy_f_oneway(*samples):
+    anova_object = f_oneway(*samples)
+    sum_object = pd.DataFrame({'F': anova_object[0], 'p': anova_object[1]}, index=[0])
+    target_name = samples[0].name
+    dt = load_datatype("https://doi.org/21.T11969/b9335ce2c99ed87735a6")
+    input_labels = []
+    inputs = []
+    for i in range(len(samples)):
+        input_label = argname('samples[' + str(i) + ']')
+        input_labels.append(input_label)
+        an_input = dt.data_item(label=input_label,
+                                has_characteristic=dt.matrix_size(
+                                    number_of_rows=len(samples[i]),
+                                    number_of_columns=1))
+        inputs.append(an_input)
+
+    soft_method = add_soft_method(dt, "scipy;f_oneway")
+    soft_method.is_implemented_by = "f_oneway(" + ",".join(input_labels) + ")"
+    target_variable = dt.component(label=target_name)
+    output = dt.data_item(label="ANOVA results",
+                          source_table=sum_object)
+    instance = dt.group_comparison(
+        label="Anova " + target_name,
+        executes=soft_method,
+        has_input=inputs,
+        targets=target_variable,
+        has_output=output)
+    json_object = to_jsonld(instance)
+    return json_object
