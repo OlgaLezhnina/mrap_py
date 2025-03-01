25ef900b38c86b374084e66f8f1945c6ff1827f4
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-03-01 11:52:11 +0100

    draft write_analytic instance

---
+++ src/mrap/write_analytic_instance.py
@@ -0,0 +1,21 @@
+from .add_input import add_input
+from .add_soft_method import add_soft_method
+from .utils import parse_code_string
+
+
+def write_analytic_instance(dt, lib, code_string, input_data, test_results):
+    schema = next(reversed(dt.__dict__.items()))[1]
+    schema_name = next(reversed(dt.__dict__.items()))[0]
+    parts = parse_code_string(code_string)
+    soft_method = add_soft_method(dt, lib, parts["fun"])
+    soft_method.is_implemented_by = code_string
+    input = add_input(dt, input_data)
+    output = dt.data_item(label=str(schema_name) + " results",
+                          source_table=test_results)
+    instance = schema(
+        label=schema_name,
+        executes=soft_method,
+        has_input=input,
+        has_output=output
+    )
+    return instance
