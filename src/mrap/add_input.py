e14fd5e494f83426c8bb4b5d5775727b61af4c73
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-03-07 16:38:33 +0100

    add_input with dict

--- src/mrap/add_input.py
+++ src/mrap/add_input.py
@@ -1,3 +1,24 @@
-def add_input(dt, inputs):
-    inputs = "TODO"
+import pandas as pd
+from collections.abc import Mapping
+from varname import argname
+
+inputs = None
+
+
+def add_input(dt, input_data):
+    global inputs
+    if isinstance(input_data, Mapping):
+        inputs = []
+        for key in input_data:
+            inp_inst = dt.data_item(label=key,
+                                    has_characteristic=dt.matrix_size(
+                                        number_of_rows="TODO1",
+                                        number_of_columns='TODO2'))
+            inputs.append(inp_inst)
+    elif isinstance(input_data, pd.DataFrame):
+        input_label = argname('input_data')
+        inputs = dt.data_item(label=input_label,
+                              has_characteristic=dt.matrix_size(
+                                  number_of_rows="TODO1",
+                                  number_of_columns='TODO2'))
     return inputs
--- src/mrap/write_analytic_instance.py
+++ src/mrap/write_analytic_instance.py
@@ -1,3 +1,5 @@
+import pandas as pd
+from varname import argname
 from .add_input import add_input
 from .add_soft_method import add_soft_method
 from .utils import parse_code_string
@@ -10,13 +12,13 @@ def write_analytic_instance(dt, schema_name,
     parts = parse_code_string(code_string)
     soft_method = add_soft_method(dt, lib, parts["fun"])
     soft_method.is_implemented_by = code_string
-    input = add_input(dt, input_data)
+    inputs = add_input(dt, input_data)
     output = dt.data_item(label=str(schema_name) + " results",
                           source_table=test_results)
     instance = schema(
         label=schema_name,
         executes=soft_method,
-        has_input=input,
+        has_input=inputs,
         has_output=output
     )
     return instance
