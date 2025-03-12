0491a5c923b8e4852f30900a7f97d2d701b2a0e2
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-03-12 11:19:35 +0100

    undo global for add_input

--- src/mrap/add_input.py
+++ src/mrap/add_input.py
@@ -2,24 +2,29 @@ import pandas as pd
 from collections.abc import Mapping
 from varname import argname
 
-inputs = None
-
 
 def add_input(dt, input_data):
-    global inputs
     if isinstance(input_data, Mapping):
         inputs = []
         for key in input_data:
-            inp_inst = dt.data_item(label=key,
-                                    has_characteristic=dt.matrix_size(
-                                        number_of_rows=len(input_data[key]),
-                                        number_of_columns=1))
+            inp_inst = dt.data_item(
+                label=key,
+                has_characteristic=dt.matrix_size(
+                    number_of_rows=len(input_data[key]),
+                    number_of_columns=1
+                )
+            )
             inputs.append(inp_inst)
     elif isinstance(input_data, pd.DataFrame):
         nrows, ncols = input_data.shape
         input_label = argname('input_data')
-        inputs = dt.data_item(label=input_label,
-                              has_characteristic=dt.matrix_size(
-                                  number_of_rows=nrows,
-                                  number_of_columns=ncols))
+        inputs = dt.data_item(
+            label=input_label,
+            has_characteristic=dt.matrix_size(
+                number_of_rows=nrows,
+                number_of_columns=ncols
+            )
+        )
+    else:
+        inputs = "TODO"
     return inputs
