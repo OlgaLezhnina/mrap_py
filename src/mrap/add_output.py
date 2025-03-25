92e9b4e2f3b6cf580617b8c7f5ad2ec045963e40
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-03-25 20:49:25 +0100

    add alg_eval output with standard dict keys

--- src/mrap/add_output.py
+++ src/mrap/add_output.py
@@ -1,3 +1,8 @@
+import pandas as pd
+from dtreg.load_datatype import load_datatype
+from .utils import standardise_keys
+
+
 def add_generic_output(dt, schema_name, test_results):
     output = dt.data_item(label=str(schema_name) + " results",
                           source_table=test_results)
@@ -5,4 +10,10 @@ def add_generic_output(dt, schema_name, test_results):
 
 
 def add_evaluation_output(dt, dictionary_results):
-    pass
+    dt = load_datatype("https://doi.org/21.T11969/5e782e67e70d0b2a022a")
+    new_dictionary = standardise_keys(dictionary_results)
+    df_results = pd.DataFrame([new_dictionary])
+    df_named = df_results.rename(index={0: 'value'})
+    output = dt.data_item(label="algorithm evaluation results",
+                          source_table=df_named)
+    return output
--- src/mrap/utils.py
+++ src/mrap/utils.py
@@ -1,3 +1,20 @@
 def parse_code_list(code_list):
     result = {"fun": code_list[1].split("(")[0]}
     return result
+
+
+def standardise_keys(old_dict):
+    new_dict = {}
+    f1_matches = ["f1", "f_1"]
+    uppercase = ["auc", "mae", "mse", "rmse"]
+    lowercase = ["accuracy", "precision", "recall"]
+    for key, value in old_dict.items():
+        if any(x in key.casefold() for x in f1_matches):
+            new_dict["F1"] = old_dict[key]
+        elif key.casefold() in uppercase:
+            new_dict[key.upper()] = old_dict[key]
+        elif key.casefold() in lowercase:
+            new_dict[key.lower()] = old_dict[key]
+        else:
+            new_dict[key] = old_dict[key]
+    return (new_dict)
