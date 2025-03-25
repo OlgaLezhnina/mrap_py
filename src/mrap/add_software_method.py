eaf2fd229c03e91fc312e457f7806a5fc6a74ad2
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-03-25 15:52:11 +0100

    adjust for "N/A" is code_list

--- src/mrap/add_software_method.py
+++ src/mrap/add_software_method.py
@@ -4,13 +4,18 @@ from .utils import parse_code_list
 
 
 def add_software_method(dt, code_list):
-    lib = code_list[0]
-    fun = parse_code_list(code_list)["fun"]
     version_py = str(sys.version)[:5]
-    version_lib = version(lib)
     software = dt.software(label="Python", version_info=version_py)
-    software_library = dt.software_library(label=lib, version_info=version_lib,
-                                           part_of=software)
-    software_method = dt.software_method(label=fun, part_of=software_library,
-                                         is_implemented_by=code_list[1])
+    if code_list == "N/A":
+        software_method = dt.software_method(part_of=software)
+    elif isinstance(code_list, list):
+        lib = code_list[0]
+        fun = parse_code_list(code_list)["fun"]
+        version_lib = version(lib)
+        software_library = dt.software_library(label=lib, version_info=version_lib,
+                                               part_of=software)
+        software_method = dt.software_method(label=fun, part_of=software_library,
+                                             is_implemented_by=code_list[1])
+    else:
+        raise TypeError("Argument code_list is of a wrong type, see Readme")
     return software_method
