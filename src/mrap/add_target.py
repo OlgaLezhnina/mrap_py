04bba007b2d34f41021a4f11620f084736e069f3
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-04-17 18:59:41 +0200

    add generic target

--- src/mrap/add_target.py
+++ src/mrap/add_target.py
@@ -22,3 +22,17 @@ def add_comparison_target(dt, code_list, input_data):
         target_name = None
     target_variable = dt.component(label=target_name)
     return target_variable
+
+
+def add_generic_target(dt, code_list, input_data):
+    """
+    Write a target instance
+
+    :param dt: an analytical schema datatype
+    :param code_list: a list of strings for library and code line, "N/A" if not given
+    :param input_data: pd.DataFrame, a dictionary, or a list with URL, n rows, and n columns
+    :return: a target instance
+    """
+    target_name = parse_code_list(code_list)["target_name"]
+    target_variable = dt.component(label=target_name)
+    return target_variable
